a61ada3c518c9374a2383c7d332012385e79db0a
Gabriel Maurici <gabriel.maurici@hotmail.com>
2021-08-31 17:37:44 -0300

    form produto, projeto react

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import ProdutoForm from "./produto_form/ProdutoForm";
+
+class App extends Component {
+  render(){
+    return(
+      <section>
+
+        <ProdutoForm />
+      </section>
+    )
+  }
+}
+
+export default App;
---
+++ front-end/src/produto_form/ProdutoForm.jsx
@@ -0,0 +1,47 @@
+import { Component } from "react";
+import { Button, TextField, Select,FormControl, MenuItem, InputLabel, Container} from '@material-ui/core';
+
+class ProdutoForm extends Component {
+    render(){
+        return(
+            <Container maxWidth="sm">
+                <form >
+                    <TextField fullWidth id="nome" label="nome" />
+                    <TextField fullWidth id="nome" label="nome" />
+                    {/* <Select
+                        displayEmpty
+                        id= "cat-select"
+                    >
+                        <MenuItem value="" disabled>
+                            Placeholder
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select> */}
+
+                    <FormControl>
+                        <InputLabel id="demo-controlled-open-select-label">Age</InputLabel>
+                        <Select
+                            labelWidth="10"
+                            autoWidth
+                            labelId="demo-controlled-open-select-label"
+                            id="demo-controlled-open-select"
+ 
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <Button></Button>
+                </form>
+            </Container>
+        )
+    }
+}
+
+export default ProdutoForm;
